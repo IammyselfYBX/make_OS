1b7fd9a1c3847178ce8147db1d2593158da13bd0
IammyselfYBX <2252176220@qq.com>
2020-05-21 11:40:00 +0800

    finish 文件系统

---
+++ test6文件系统/test4目录操作/test3删除目录/Env/1.bximage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+bximage -hd -mode="flat" -size=10 -q hd10M.img
+bximage -hd -mode="flat" -size=15 -q hd15M.img
+
+# bximage的参数
+# -fd 创建软盘
+# -hd 创建硬盘
+# -mode 创建硬盘的类型，有 flat sparse growing
+# -size 指创建多大的硬盘，以 MB 为单位。
+# -q 以静默模式创建，创建过程中不会和用户交互。
